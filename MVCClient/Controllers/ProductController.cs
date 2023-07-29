18fed4f4c09a8d000ddf1a4f6fa8b55952c3eda0
neominalsolutions <neominal.solutions@gmail.com>
2023-07-29 13:00:04 +0300

    Add project files.

---
+++ MVCClient/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCClient.Models;
+using System.Net;
+
+namespace MVCClient.Controllers
+{
+  public class ProductController : Controller
+  {
+    public async Task<IActionResult> Index()
+    {
+      // c# kendi yönetemediği dış kayanaklara ait maliyetli bağlantılarda using keyword kullanarak
+      // kaynak yönetimini c# otomatik yapmasına imkan sağlarız. (API işlemleri, Database işlemleri, File İşlermleri maliyetli işlemler olduğundan dolayı using kod blogu içerisinde bağlantı kuruyoruz)
+      using (var client = new HttpClient()) // Api bağlanmak için c# HttpClient sınıfı kullanıyoruz
+      {
+        client.BaseAddress = new Uri("https://localhost:7091/"); // bağlantılı olan sunucu adresi
+        client.Timeout = TimeSpan.FromSeconds(30); // 30sn sunucu erişilemezse istek timeout düşsün
+
+        var response =  await client.GetAsync("api/Products"); // endpoint veri çekeceğimiz uç
+
+        if(response.StatusCode == HttpStatusCode.OK) // 200 dönmüş ise o zaman veri çekebilirim
+        {
+          // git bunu apiden json formatında oku json formatındaki veriyi ProductModel tipine çevir
+
+          var productJsonString = response.Content.ReadAsStringAsync().Result;
+
+          var model = System.Text.Json.JsonSerializer.Deserialize<List<ProductModel>>(productJsonString);
+
+          return View(model);
+        }
+        else
+        {
+          return View("Error");
+        }
+      }
+    }
+  }
+}
---
+++ MVCClient/Models/ProductModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MVCClient.Models
+{
+  public class ProductModel
+  {
+    // api dan çekilecek olan verile bu modeli dolduracak.
+
+    [JsonPropertyName("id")]
+    public int Id { get; set; } // json properyNamelere göre model açtık 
+
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("price")]
+    public decimal Price { get; set; }
+
+    [JsonPropertyName("stock")]
+    public short? Stock { get; set; }
+
+
+  }
+}
